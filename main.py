53c3580383c23ad6694d99b60a3599112d75d521
EkberHasanov <ekber.022004@gmail.com>
2023-06-16 23:50:09 +0400

    Working on to implement translategram and improve translategram based on this codebase

--- main.py
+++ main.py
@@ -2,50 +2,49 @@ import logging
 import openai
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from auto_translategram import PythonTelegramBotTranslator, MtranslateTranslatorService
+
+translator = PythonTelegramBotTranslator(MtranslateTranslatorService)
+
 
 logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
 
 logger = logging.getLogger(__name__)
 
 message_history = list()
-openai.api_key = 'YOUR OPENAI API KEY'
-OWNER_ID = 'YOUR USER ID'
+openai.api_key = "YOUR OPENAI API KEY"
 
 
 def chat(inp: str, role="user") -> str:
+    """
     global message_history
-    message_history.append({'role': role, 'content': inp})
+    message_history.append({"role": role, "content": inp})
     completion = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=message_history
-        )
+        model="gpt-3.5-turbo", messages=message_history
+    )
     reply_content = completion.choices[0].message.content
-    message_history.append({'role': 'assistant', 'content': reply_content})
+    message_history.append({"role": "assistant", "content": reply_content})
     return reply_content
+    """
+    if len(inp) > 15:
+        return "Your text is longer than 15"
+    return "Your text is less than 15"
 
 
-async def ask(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    if str(update.message.chat_id) != OWNER_ID:
-        await context.bot.send_message(
-                chat_id=update.effective_chat.id,
-                text="I only help my masters!"
-            )
-    else:
-        text_inp = ' '.join(context.args)
-        answer = chat(inp=text_inp)
-        await context.bot.send_message(
-                chat_id=update.effective_chat.id,
-                text=answer
-            )
+@translator.handler_translator(
+    message="Currently I am thinking, what should I have done!"
+)
+async def ask(update: Update, context: ContextTypes.DEFAULT_TYPE, message: str) -> None:
+    # text_inp = " ".join(context.args)
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=message)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     logger.info("Application is started...")
-    application = ApplicationBuilder().token('YOUR BOT TOKEN').build()
-    ask_handler = CommandHandler('ask', ask)
+    application = ApplicationBuilder().token("YOUR BOT TOKEN").build()
+    ask_handler = CommandHandler("ask", ask)
     application.add_handler(ask_handler)
 
     application.run_polling()
